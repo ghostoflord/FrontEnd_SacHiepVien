1f5b68c5dcc1f4ff01f41c41e6b7dbc7ae0f53e0
ghostoflord <ghost2892003zzz@gmail.com>
2025-02-15 20:41:54 +0700

    update tree project

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import User from "./pages/User";
+import ErrorPage from "./error/ErrorPage";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/users",
+    element: <User />,
+    errorElement: <ErrorPage />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/User.jsx
@@ -0,0 +1,8 @@
+const User = () => {
+    return (
+        <>
+            from user page
+        </>
+    )
+}
+export default User;
