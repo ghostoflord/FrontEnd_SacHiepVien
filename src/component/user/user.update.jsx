495ab672af16b3376d2ee6ad12eca1aaccbad66c
ghostoflord <ghost2892003zzz@gmail.com>
2025-02-19 21:19:53 +0700

    update

---
+++ src/component/user/user.update.jsx
@@ -0,0 +1,111 @@
+import { Modal, Input, notification, Select, Form, InputNumber } from 'antd';
+const { Option } = Select;
+
+const CreateUserModal = (props) => {
+    const {
+        access_token, getData,
+        isCreateModalOpen, setIsCreateModalOpen
+    } = props;
+    // const access_token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJwZXJtaXNzaW9uIjpbIlJPTEVfVVNFUl9DUkVBVEUiLCJST0xFX1VTRVJfVVBEQVRFIl0sImV4cCI6MTgyNjM0NzE2OSwiaWF0IjoxNzM5OTQ3MTY5LCJ1c2VyIjp7ImlkIjoxLCJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsIm5hbWUiOm51bGx9fQ.9XM8xgGmTD94wl4Q0RfWRvWDA1ns0UX3ehPCX43XvocsUJxyzVIal9tJQv6lyjvwtmeQv7XZzemXIlGqGxUJFA";
+
+    const [form] = Form.useForm();
+
+    const handleCloseCreateModal = () => {
+        form.resetFields();
+        setIsCreateModalOpen(false);
+    };
+
+    const onFinish = async (values) => {
+        console.log('Success:', values);
+        const { userName, email, password, gender, address } = values;
+
+        const data = { userName, email, password, gender, address };
+        const res = await fetch("http://localhost:8080/api/v1/users", {
+            method: "POST",
+            headers: {
+                'Authorization': `Bearer ${access_token}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        });
+
+        const d = await res.json();
+        if (d.data) {
+            // success  
+            await getData();
+            notification.success({
+                message: "Tạo mới user thành công.",
+            });
+            handleCloseCreateModal();
+        } else {
+            notification.error({
+                message: "Có lỗi xảy ra",
+                description: JSON.stringify(d.message)
+            });
+        }
+    };
+
+    return (
+        <Modal
+            title="Add new user"
+            open={isCreateModalOpen}
+            onOk={() => form.submit()}
+            onCancel={() => handleCloseCreateModal()}
+            maskClosable={false}
+        >
+            <Form
+                name="basic"
+                onFinish={onFinish}
+                layout="vertical"
+                form={form}
+            >
+                <Form.Item
+                    style={{ marginBottom: 5 }}
+                    label="User Name"
+                    name="userName"
+                    rules={[{ required: true, message: 'Please input your userName!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    style={{ marginBottom: 5 }}
+                    label="Email"
+                    name="email"
+                    rules={[{ required: true, message: 'Please input your email!' }]}
+                >
+                    <Input type='email' />
+                </Form.Item>
+
+                <Form.Item
+                    style={{ marginBottom: 5 }}
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    style={{ marginBottom: 5 }}
+                    label="Gender"
+                    name="gender"
+                    rules={[{ required: true, message: 'Please input your gender!' }]}
+                >
+                    <Input style={{ width: "100%" }} />
+                </Form.Item>
+
+                <Form.Item
+                    style={{ marginBottom: 5 }}
+                    label="Address"
+                    name="address"
+                    rules={[{ required: true, message: 'Please input your address!' }]}
+                >
+                    <Input />
+                </Form.Item>
+            </Form>
+        </Modal>
+    );
+};
+
+export default CreateUserModal;
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import { notification, Popconfirm, Table } from "antd";
-import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import { Button, notification, Popconfirm, Table } from "antd";
+import { DeleteOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons';
+import CreateUserModal from "../component/user/user.update";
 const User = () => {
     const [listUsers, setListUsers] = useState([]);
+    const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
     const access_token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJwZXJtaXNzaW9uIjpbIlJPTEVfVVNFUl9DUkVBVEUiLCJST0xFX1VTRVJfVVBEQVRFIl0sImV4cCI6MTgyNjM0NzE2OSwiaWF0IjoxNzM5OTQ3MTY5LCJ1c2VyIjp7ImlkIjoxLCJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsIm5hbWUiOm51bGx9fQ.9XM8xgGmTD94wl4Q0RfWRvWDA1ns0UX3ehPCX43XvocsUJxyzVIal9tJQv6lyjvwtmeQv7XZzemXIlGqGxUJFA";
 
     useEffect(() => {
@@ -65,11 +67,33 @@ const User = () => {
     // lift-up state 
     return (
         <div style={{ padding: "20px" }}>
+            <div style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center"
+            }}>
+                <h2>Table Users</h2>
+                <div>
+                    <Button
+                        icon={<PlusOutlined />}
+                        type={"primary"}
+                        onClick={() => setIsCreateModalOpen(true)}
+                    >Add new</Button>
+                </div>
+
+            </div>
             {/* <UserForm loadUser={loadUser} /> */}
             <Table
                 columns={columns}
                 dataSource={listUsers} // Sử dụng dataSource để truyền danh sách dữ liệu
                 rowKey="id" // chỉ định một khóa duy nhất cho từng hàng  
+            />
+
+            <CreateUserModal
+                access_token={access_token}
+                getData={getData}
+                isCreateModalOpen={isCreateModalOpen}
+                setIsCreateModalOpen={setIsCreateModalOpen}
             />
 
         </div>
